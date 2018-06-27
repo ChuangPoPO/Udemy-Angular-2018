14ce9a9784cd9b99625e8a0412f78e8aa614a1f1
ChuangPoPO <s842563@gmail.com>
2018-06-27 10:17:49 +0800

    4-12 1. 手動刪除 page1

--- demo1/src/app/app.module.ts
+++ demo1/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Page1Component
+    Page2Component
   ],
   imports: [
     BrowserModule
