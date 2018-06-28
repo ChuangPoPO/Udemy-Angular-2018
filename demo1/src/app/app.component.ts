a1b8c04a1b3832f7f7e547b792018b89a84d9f54
ChuangPoPO <s842563@gmail.com>
2018-06-28 08:54:16 +0800

    5-21 簡化事件傳入方式

--- demo1/src/app/app.component.ts
+++ demo1/src/app/app.component.ts
@@ -13,11 +13,9 @@ export class AppComponent {
   constructor() {
   }
 
-  changeTitle($event : MouseEvent) {
-    if ($event.altKey) {
+  changeTitle(altKey: boolean) {
+    if (altKey) {
       this.title = 'The Will Will Web';
     }
-
-    console.log($event);
   }
 }
