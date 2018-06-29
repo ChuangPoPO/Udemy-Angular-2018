5d479beff65652e13343d54a053a5a6bdab8873c
ChuangPoPO <s842563@gmail.com>
2018-06-29 16:56:15 +0800

    6-41 create article-list component - include article-header & article-body components - cd to article folder to create use `ng g c article-list` - remember to exports ArticleListComponent in ArticleModule

--- demo1/src/app/article/article.module.ts
+++ demo1/src/app/article/article.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ArticleListComponent } from './article-list/article-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [ArticleListComponent],
+  exports: [ArticleListComponent]
 })
 export class ArticleModule { }
