902438cd626b76bd76f1a42f0052af8977bd416f
ChuangPoPO <s842563@gmail.com>
2018-06-29 17:04:05 +0800

    6-41 create article-header component - in article folder to create - moves html - add directive

--- demo1/src/app/article/article.module.ts
+++ demo1/src/app/article/article.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArticleListComponent } from './article-list/article-list.component';
+import { ArticleHeaderComponent } from './article-header/article-header.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ArticleListComponent],
+  declarations: [
+    ArticleListComponent,
+    ArticleHeaderComponent],
   exports: [ArticleListComponent]
 })
 export class ArticleModule { }
